0c5e142be34509090a1b9f7c1220c7041a6290ee
blackmoja <limlim980625@gmail.com>
2022-11-09 18:10:36 +0900

    feat: rollup-react-component 라이브러리 기본설정 진행

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,37 @@
-// * RollUp 플러그인
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import terser from "@rollup/plugin-terser";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { babel } from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
 
-const extensions = ["js", "jsx", "ts", "tsx", "mjs"];
+// import type { RollupOptions } from "rollup";
 
-const config = [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        dir: "dist",
-        format: "cjs",
-        exports: "named",
-        sourcemap: true,
-        preserveModules: true,
-      },
-      // {
-      //   file: "dist/esm/index.js",
-      //   format: "esm",
-      //   sourcemap: true,
-      // },
-    ],
-    external: [/@babel\/runtime/, "react", "react-dom"],
-    plugins: [
-      babel({
-        include: ["src/**/*"],
-        exclude: "node_modules/**",
-        babelHelpers: "runtime",
-        extensions,
-      }),
-      nodeResolve({ extensions }),
-      commonjs(),
-      peerDepsExternal(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-    ],
-  },
-];
-
-export default config;
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    nodeResolve({
+      browser: true,
+    }),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    babel({
+      extensions: ["ts", "tsx"],
+      include: ["src/**/*"],
+      babelHelpers: "runtime",
+      // babelHelpers: "runtime", TODO 추후 runtime bable 추가 예정 @Liam
+    }),
+  ],
+}; // as RollupOptions;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 interface ButtonProps {
   label: string;
 }
