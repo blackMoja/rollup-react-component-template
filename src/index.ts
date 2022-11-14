5a2f167a8f42515716a2e28644ee57268da47ee9
blackmoja <limlim980625@gmail.com>
2022-11-14 14:41:01 +0900

    feat: 사용하지 않는 컴포넌트 export 로직 제거

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-export { default as Card } from "./Card";
-export * from "./Card";
-
-export { default as Input } from "./Input";
-export * from "./Input";
-
 export { default as Typography } from "./Typography";
 export * from "./Typography";
