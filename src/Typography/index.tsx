b1f6456c8f444d714351f57c85c06a816de56b53
blackmoja <limlim980625@gmail.com>
2022-11-14 12:26:35 +0900

    feat: Typography 컴포넌트 추가 & path alias 제거

---
+++ src/Typography/index.tsx
@@ -0,0 +1,46 @@
+import type { BaseColor } from "../../src/foundation/colors/type";
+import type {
+  TypographyAligns,
+  TypographyVariants,
+} from "../../src/foundation/typography/type";
+import React from "react";
+
+interface TypographyProps {
+  variant: TypographyVariants;
+  color?: BaseColor;
+  align?: TypographyAligns;
+  as?: keyof JSX.IntrinsicElements;
+  sm?: TypographyVariants;
+  md?: TypographyVariants;
+  lg?: TypographyVariants;
+  children?: React.ReactNode;
+}
+
+const Typography: React.FC<TypographyProps> = ({
+  variant,
+  color = "text-primary",
+  as: Component = "p",
+  align,
+  sm,
+  md,
+  lg,
+  children,
+}) => {
+  return (
+    <Component
+      className={[
+        `sg-typo-${variant}`,
+        `sg-color-${color}`,
+        align && `sg-typo-align-${align}`,
+        sm && `sm:sg-typo-${sm}`,
+        md && `md:sg-typo-${md}`,
+        lg && `lg:sg-typo-${lg}`,
+      ]
+        .filter((c) => c)
+        .join(" ")}
+    >
+      {children}
+    </Component>
+  );
+};
+export default Typography;
