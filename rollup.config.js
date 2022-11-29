5153b2bb721a551960a6af801fdb7ccecb1dc928
blackmoja <limlim980625@gmail.com>
2022-11-29 15:04:20 +0900

    feat: 롤업 설정 마무리

--- rollup.config.js
+++ rollup.config.js
@@ -1,77 +1,77 @@
 import { defineConfig } from 'rollup';
-import babel from '@rollup/plugin-babel';
+import ttypescript from 'ttypescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import ttypescript from 'ttypescript';
+import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
-import alias from '@rollup/plugin-alias';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import progress from 'rollup-plugin-progress';
 import copy from 'rollup-plugin-copy';
 import makePackageJson from 'rollup-plugin-generate-package-json';
-import progress from 'rollup-plugin-progress';
 import packageJson from './package.json';
 
-const extensions = ['.js', '.jsx', '.ts', '.tsx', '.mjs'];
+const isProduction = process.env.NODE_ENV === 'production';
 
-export default defineConfig([
-  {
-    external: [/@babel\/runtime/, 'react', 'react-dom', 'tslib'],
-    input: 'src/index.ts',
-    output: [
-      {
-        dir: './dist',
-        format: 'cjs',
-        preserveModules: true,
-        exports: 'named',
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-      },
-    ],
-    plugins: [
-      progress({ clearLine: false }),
-      nodeResolve({ extensions }),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
-        extensions,
-        plugins: ['@babel/plugin-transform-runtime'],
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-typescript',
-          '@babel/preset-react',
-        ],
-      }),
-      commonjs(),
-      peerDepsExternal(),
-      typescript({
-        tsconfig: 'tsconfig.json',
-        typescript: ttypescript,
-      }),
-      copy({ targets: [{ src: './src/style/index.css', dest: './dist' }] }),
-      makePackageJson({
-        inputFolder: './',
-        outputFolder: 'dist',
-        baseContents: (pkg) => ({
-          name: pkg.name,
-          version: pkg.version,
-          repository: pkg.repository,
-          engines: pkg.engines,
-          main: pkg.main.replace('dist/', ''),
-          module: pkg.module.replace('dist/', ''),
-          browser: pkg.browser.replace('dist/', ''),
-          types: pkg.types.replace('dist/', ''),
-          peerDependencies: pkg.peerDependencies,
-        }),
-      }),
-      alias({
-        entries: {
-          '@/*': './src/*',
-        },
-      }),
-      terser(),
-    ],
-  },
-]);
+const input = './src/index.ts';
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const external = [/@babel\/runtime/, 'react', 'react-dom', 'tslib'];
+const progressOption = { clearLine: false };
+const nodeResolveOption = { extensions };
+const babelOption = {
+  extensions,
+  babelHelpers: 'runtime',
+  exclude: 'node_modules/**',
+  plugins: ['@babel/plugin-transform-runtime'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+};
+const tsOption = {
+  tsconfig: 'tsconfig.json',
+  typescript: ttypescript,
+};
+const copyOption = {
+  targets: [{ src: './src/style/index.css', dest: './dist' }],
+};
+const makePackageJsonOption = {
+  inputFolder: './',
+  outputFolder: 'dist',
+  baseContents: (pkg) => ({
+    main: pkg.main.replace('dist/', ''),
+    module: pkg.module.replace('dist/', ''),
+    browser: pkg.browser.replace('dist/', ''),
+    types: pkg.types.replace('dist/', ''),
+    ...pkg,
+  }),
+};
+
+export default defineConfig({
+  external,
+  input,
+  output: [
+    {
+      dir: './dist',
+      format: 'cjs',
+      preserveModules: true,
+      exports: 'named',
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    progress(progressOption),
+    makePackageJson(makePackageJsonOption),
+    nodeResolve(nodeResolveOption),
+    babel(babelOption),
+    commonjs(),
+    peerDepsExternal(),
+    typescript(tsOption),
+    copy(copyOption),
+    isProduction && terser(),
+  ],
+});
